9cebf5c309537db4c2ae07cd93f148ac22d47707
Saloni50999 <salonikushwaha7398@gmail.com>
2025-03-18 12:20:24 +0530

    Add modified  files

--- controllers/auth-cont.js
+++ controllers/auth-cont.js
@@ -1,7 +1,8 @@
  // in an express js application, a "controller" refers to a part of your code that is responsible for handling the applications's logic.
  // Controllers are typically used to process incoming requests,interact with models(data sources),and send responses back to client. 
  // They help organizee your application by separating concerns and following the MVC(Model-view-controller) design pattern
- 
+ const User = require("../models/user-model");
+ const bcrypt = require("bcrypt");
 // *___________________
 // Home logic 
 // *___________________
@@ -20,7 +21,22 @@ const home = async (req,res)=>{
 const register = async (req,res)=>{
     try{
         console.log(req.body);
-        res.status(200).json({message:req.body});
+        const {username,email,phone,password} = req.body;
+
+        const userExist = await User.findOne({email});
+        if(userExist){
+            return res.status(400).json({msg:"email already registered"});
+        }
+        //hash the password
+        // const saltRound = 10;
+        // const hash_password = await bcrypt.hash(password,saltRound);
+        const userCreated = await User.create({
+            username,
+            email,
+            phone,
+            password 
+        });
+        res.status(201).json({msg:userCreated});
     }catch(error){
        res.status(400).json({msg:"page not found"})
     }
